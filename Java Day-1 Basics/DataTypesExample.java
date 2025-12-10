47f9e2cb73f843c154348fdc7b915f2b6d90a4a9
hanumavathip06 <hanumavathipothuganti06@gmail.com>
2025-12-10 15:25:25 +0530

    Datatypes

---
+++ Java Day-1 Basics/DataTypesExample.java
@@ -0,0 +1,24 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+
+        byte a = 10;               // 1 byte
+        short b = 200;             // 2 bytes
+        int c = 5000;              // 4 bytes
+        long d = 100000L;          // 8 bytes
+
+        float e = 5.75f;           // 4 bytes
+        double f = 19.99;          // 8 bytes
+
+        char g = 'A';              // 2 bytes
+        boolean h = true;          // 1 bit
+
+        System.out.println("byte : " + a);
+        System.out.println("short : " + b);
+        System.out.println("int : " + c);
+        System.out.println("long : " + d);
+        System.out.println("float : " + e);
+        System.out.println("double : " + f);
+        System.out.println("char : " + g);
+        System.out.println("boolean : " + h);
+    }
+}
