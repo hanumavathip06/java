d843cf99482feb86b9086839bcbd0a1ea37ac529
hanumavathip06 <hanumavathipothuganti06@gmail.com>
2025-12-10 15:28:26 +0530

    Variables

---
+++ Java Day-1 Basics/VariablesExample.java
@@ -0,0 +1,26 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        
+        // Variable declaration and initialization
+        int age = 21;
+        double salary = 35000.50;
+        char grade = 'A';
+        String name = "Ramesh";
+        boolean isStudent = true;
+
+        // Using variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student? " + isStudent);
+
+        // Updating variables
+        age = age + 1;
+        salary = salary + 5000;
+
+        System.out.println("Updated Age: " + age);
+        System.out.println("Updated Salary: " + salary);
+    }
+}
+
